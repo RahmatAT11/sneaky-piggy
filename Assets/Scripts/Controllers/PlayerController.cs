22d1ef3e83985f458816007260611cd2eb1b5c25
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2021-11-15 15:33:45 +0700

    Player can walk and run
    pemain bisa berjalan dan berlari
    untuk sementara, berjalan menggunakan wasd dan berlari menekan tombol left shift

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,8 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : BaseCharController
 {
-    
+    private void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        
+        MovementDirection.Set(xAxis, yAxis, 0f);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+            return;
+        }
+
+        isSprinting = false;
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+        Turning();
+        Sprinting();
+    }
 }
