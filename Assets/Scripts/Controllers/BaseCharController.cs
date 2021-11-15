8c999a2b4c2af5578df072ac8f952021d5ae0978
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2021-11-15 15:24:57 +0700

    Create new script specific for player
    membuat skrip PlayerController yang inherit dari BaseCharController
    setup gameobject player agar bisa digerakkan
    membuat mekanik untuk berjalan dan berlari

--- Assets/Scripts/Controllers/BaseCharController.cs
+++ Assets/Scripts/Controllers/BaseCharController.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseCharController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected float MovementSpeed = 10.0f;
+    protected Vector3 MovementDirection;
+
+    protected Rigidbody2D Rigidbody2D;
+    
+    protected bool isSprinting;
+    public bool IsSprinting
     {
-        
+        get
+        {
+            return isSprinting;
+        }
+        set
+        {
+            isSprinting = value;
+        }
+    }
+    private float _sprintSpeedMultiplier = 1.7f;
+
+    protected virtual void Walking()
+    {
+        // menggerakkan player ke vector
+        Rigidbody2D.velocity = MovementDirection * MovementSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Turning()
     {
+        // rotate if vector _movementDirection is not zero
+        if (MovementDirection == Vector3.zero) return;
         
+        float angle = Vector2.SignedAngle(transform.up, MovementDirection);
+        Rigidbody2D.MoveRotation(Rigidbody2D.rotation + angle);
+    }
+    
+    protected virtual void Sprinting()
+    {
+        if (IsSprinting)
+        {
+            Rigidbody2D.velocity = MovementDirection * (MovementSpeed * _sprintSpeedMultiplier);
+        }
+    }
+
+    protected virtual void SetRotationToZero()
+    {
+        transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
     }
 }
