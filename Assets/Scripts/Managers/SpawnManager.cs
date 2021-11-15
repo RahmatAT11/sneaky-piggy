eba7f57636239d33f9ec0cad867dee97b0c136f0
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2021-11-15 15:11:42 +0700

    player spawn at spawn point
    pada awal permainan, pemain akan di spawn pada titik spawn point.

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private BaseCharController playerPrefab;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Instantiate(playerPrefab, transform.position, playerPrefab.transform.rotation);
     }
 }
