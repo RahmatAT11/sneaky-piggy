052d8b4f34323bcd33bb997a234c7a18325fbb49
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2021-11-16 12:58:50 +0700

    Set up placeholder path for enemy

--- Assets/Scripts/Controllers/NonPlayerController.cs
+++ Assets/Scripts/Controllers/NonPlayerController.cs
@@ -1,8 +1,23 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NonPlayerController : BaseCharController
 {
-    
+    [SerializeField] private List<Transform> defaultPath;
+    private PathsController _paths;
+
+    private int _currentPathIndex = 0;
+
+    private void Awake()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        _paths = FindObjectOfType<PathsController>();
+        defaultPath = _paths.NpcPath;
+    }
+
+    private void Start()
+    {
+        transform.position = defaultPath[_currentPathIndex].transform.position;
+    }
 }
