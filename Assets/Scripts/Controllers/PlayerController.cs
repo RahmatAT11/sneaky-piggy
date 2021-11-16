53d498fc95ab27eaaf60e26a3776c2e140aba923
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2021-11-17 06:09:19 +0700

    Player can move with joystick

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerController : BaseCharController
 {
+    private GameJoystickController _joystick;
+    
     private void Awake()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        _joystick = FindObjectOfType<GameJoystickController>();
     }
 
     private void Update()
     {
-        float xAxis = Input.GetAxis("Horizontal");
-        float yAxis = Input.GetAxis("Vertical");
+        /*float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");*/
+
+        float xAxis = _joystick.InputHorizontal();
+        float yAxis = _joystick.InputVertical();
         
         MovementDirection.Set(xAxis, yAxis, 0f);
 
--- Assets/Scripts/Controllers/Sprint Button/GameSprintController.cs
+++ Assets/Scripts/Controllers/Sprint Button/GameSprintController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameJoystickController : MonoBehaviour
+public class GameSprintController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
