1e282258224b8a20118932a893da96c51d20653d
Dany Zaky <danyzakydany123@gmail.com>
2021-11-23 20:44:46 +0700

    pause menu

---
+++ Assets/Scripts/Managers/ButtonManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void pauseButton()
+    {
+
+    }
+
+    public void resumeButton()
+    {
+
+    }
+
+    public void restartButton()
+    {
+
+    }
+
+    public void settingButton()
+    {
+
+    }
+
+    public void mainMenuButton()
+    {
+
+    }
+
+    public void adjustButton()
+    {
+
+    }
+
+    public void storeButton()
+    {
+
+    }
+
+    public void soundOnButton()
+    {
+
+    }
+
+    public void soundOffButton()
+    {
+
+    }
+}
